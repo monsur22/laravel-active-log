f3a09c0e487de011010e460d9d8cc01ba04f6356
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-08-10 11:39:52 +0600

    log insert

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Spatie\Activitylog\Models\Activity;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Activity::all();
+
+    // return view('welcome');
 });
